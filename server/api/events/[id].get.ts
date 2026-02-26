12be0ed61e23991c356cdfe3205c80eae82e8d7f
Gilfred Dgb <gilfredmawulomdgb@gmail.com>
2026-02-26 16:09:13 +0100

    correction de l'incrémentation de la vue: si la meme personne visite le meme event la vue s'incrémente qu'une seule fois

--- server/api/events/[id].get.ts
+++ server/api/events/[id].get.ts
@@ -1,7 +1,9 @@
 import { prisma } from "~~/server/utils/prisma";
+import { getRequestIP } from "h3";
 
 export default defineEventHandler(async (event) => {
   const id = getRouterParam(event, "id");
+  const ip = getRequestIP(event);
 
   if (!id) {
     throw createError({
@@ -10,14 +12,34 @@ export default defineEventHandler(async (event) => {
     });
   }
 
-  try {
-    const eventData = await prisma.event.update({
-      where: { id },
-      data: {
-        views: {
-          increment:1,
-        }
+// vérifier si la vue existe déja
+const existingView = await prisma.eventView.findFirst({
+  where: {
+    eventId: id, //0196740100
+    ipAddress: ip,
+  },
+});
+
+if (!existingView) {
+  await prisma.eventView.create({
+    data: {
+      eventId: id,
+      ipAddress: ip,
+    },
+  });
+
+  await prisma.event.update({
+    where: { id },
+    data: {
+      views: {
+        increment: 1,
       },
+    },
+  });
+}
+
+const eventData = await prisma.event.findUnique({
+      where: { id },
       include: {
         ville: true,
         category: true,
@@ -32,17 +54,13 @@ export default defineEventHandler(async (event) => {
         },
       },
     });
-    return eventData;
-  } catch (error: any) {
-    if (error.code === "P2025") {
+    
+    if (!eventData) {
       throw createError({
         statusCode: 404,
         statusMessage: "Événement non trouvé",
       });
-    }
-    throw createError({
-      statusCode: 500,
-      statusMessage: "Erreur lors de la récupération de l'événement",
-    });
-  }
-});
+    } 
+
+    return eventData;
+  })

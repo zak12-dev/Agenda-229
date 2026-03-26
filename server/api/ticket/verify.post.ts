63d4a269c6edbc84e8d839600fbf7a43bbf1c5bb
Gilfred Dgb <gilfredmawulomdgb@gmail.com>
2026-03-26 03:01:46 +0100

    finalisation d'endpoint pour la vérification de ticket

---
+++ server/api/ticket/verify.post.ts
@@ -0,0 +1,92 @@
+import { prisma } from '../../utils/prisma'
+
+export default defineEventHandler(async (event) => {
+  try {
+    const body = await readBody(event)
+    const { token, userId } = body
+
+    if (!token || !userId) {
+      return {
+        success: false,
+        message: 'Token ou userId manquant'
+      }
+    }
+
+    const ticket = await prisma.ticket.findUnique({
+      where: { token },
+      include: {
+        event: true,
+        user: true
+      }
+    })
+
+    if (!ticket) {
+      return {
+        success: false,
+        message: 'Ticket invalide'
+      }
+    }
+
+    // ⏰ expiré
+    if (new Date() > ticket.expiresAt) {
+      await prisma.scanLog.create({
+        data: {
+          ticketId: ticket.id,
+          scannedBy: userId,
+          scanResult: 'EXPIRED'
+        }
+      })
+
+      return { success: false, message: 'Ticket expiré' }
+    }
+
+    // 🚫 déjà utilisé
+    if (ticket.usedCount >= ticket.maxUsage) {
+      await prisma.scanLog.create({
+        data: {
+          ticketId: ticket.id,
+          scannedBy: userId,
+          scanResult: 'REFUSED'
+        }
+      })
+
+      return { success: false, message: 'Ticket déjà utilisé' }
+    }
+
+    // ✅ OK
+    await prisma.ticket.update({
+      where: { id: ticket.id },
+      data: {
+        usedCount: { increment: 1 }
+      }
+    })
+
+    await prisma.ticketValidation.create({
+        data: {
+            ticketId: ticket.id,
+            validatedBy: userId
+        }
+    })
+
+    await prisma.scanLog.create({
+      data: {
+        ticketId: ticket.id,
+        scannedBy: userId,
+        scanResult: 'VALID'
+      }
+    })
+
+    return {
+      success: true,
+      message: 'Ticket valide '
+    }
+
+  } catch (error) {
+    console.error(error)
+
+    return {
+      success: false,
+      message: 'Erreur serveur'
+    }
+  }
+})

46cccf2ff536a14dafd4acbca18edee23ce94801
Gilfred Dgb <gilfredmawulomdgb@gmail.com>
2026-03-30 09:37:13 +0100

    acceptation de scanner le codeQr

--- server/modules/ticket/services/generateQr.ts
+++ server/modules/ticket/services/generateQr.ts
@@ -2,7 +2,7 @@ import QRCode from 'qrcode'
 
 export const generateQr = async (token: string) => {
 
-  const url = `http://localhost:3000/api/ticket/verify?token=${token}`
+  const url = `http://localhost:3000/scan?token=${token}`
 
   return await QRCode.toDataURL(url, {
     errorCorrectionLevel: 'H',
